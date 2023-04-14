34445111bdc2add6ec0381b8a056cb1bcc4fa67b
madjin <32600939+madjin@users.noreply.github.com>
2023-04-14 00:21:33 -0400

    add table.py

---
+++ table.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import math
+from pathlib import Path
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python table.py directory/")
+        sys.exit(1)
+
+    directory = sys.argv[1]
+    pairs = {}
+
+    for glb in Path(directory).glob("*.glb"):
+        base = glb.stem
+        png = glb.with_suffix(".png")
+        if png.exists():
+            pairs[base] = (glb, png)
+
+    count = len(pairs)
+    size = int(math.sqrt(count))
+
+    print("|", end="")
+    for i in range(size):
+        print(f" {i + 1} |", end="")
+    print()
+
+    print("|", end="")
+    for i in range(size):
+        print(" --- |", end="")
+    print()
+
+    index = 0
+    for key, value in pairs.items():
+        if index % size == 0:
+            print("|", end="")
+
+        glb, png = value
+        print(f" [![{key}]({png})]({glb}) |", end="")
+
+        index += 1
+
+        if index % size == 0:
+            print()
+
+    print()
+
+if __name__ == "__main__":
+    main()
